6e2007b3ca99f33754e6932e5e466498630a6eaa
giteup <dharman@4harman.com>
2013-10-25 17:30:07 -0500

    2013_10_25_Update

--- application.js
+++ application.js
@@ -1,6 +1,11 @@
 $(document).ready (function () {
-	$(".basecamp").click (function () {
+	$(".basecamp").hover (function () {
 	$(".t, .t-bc").toggleClass ("hide");
-
-}):
+});
+	$(".highrise").hover (function () {
+	$(".t, .t-hr").toggleClass ("hide");
+});
+	$(".campfire").hover (function () {
+	$(".t, .t-cf").toggleClass ("hide");
+});
 });
